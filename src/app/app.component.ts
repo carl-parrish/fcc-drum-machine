451085858c5b2a7d62477ef7e430df9d5113b96a
Carl Parrish <carl.parrish@gmail.com>
2020-03-15 23:17:45 +0000

    Grab audio by event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,36 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 })
 export class AppComponent {
   @ViewChild('display', {static: true}) display: ElementRef;
+
   drumKeys: {[key: string]: string} = {
-    'Q': 'RDM_RAW_SY1-Tom01.wav',
+    'Q': 'RDM_Raw_SY1-Tom01.wav',
     'W': 'RDM_Raw_MT40-Kick01.wav',
-    'E': 'RDM_Raw_MT40-ClHat.wav'
+    'E': 'RDM_Raw_MT40-ClHat.wav',
+    'A': 'RDM_Raw_SR88-Perc.wav',
+    'S': 'RDM_Raw_MT40-Kick02.wav',
+    'D': 'RDM_Raw_MT40-OpHat02.wav',
+    'Z': 'RDM_Raw_SY1-Snr01.wav',
+    'X': 'RDM_Raw_SY1-Kick03.wav',
+    'C': 'RDM_Raw_SY1-HfHat.wav'
   };
+
   baseURL = 'https://sourcecodesamurai.s3-us-west-2.amazonaws.com/media/';
   title = 'drum-machine';
 
-  onTap(key: string, fileName:string){
-    this.playMedia(fileName);
+  onTap(key: string, event: Event){
+    let audioObject = (event.target as Element).firstElementChild as HTMLAudioElement;
     this.updateDisplay(key);
+    this.playMedia(audioObject);
+
   }
 
-  playMedia(fileName: string){
-    let audio = new Audio;
-    audio.src = this.baseURL + 'RDM_Raw_SY1-Tom01.wav';
+  playMedia(audio: HTMLAudioElement){
     audio.play();
   }
 
   updateDisplay(key:string){
     this.display.nativeElement.innerHTML = key;
   }
+
+  noSort = (a, b) => 1;
 }
