c49d5eef20bba4a33ac018e37ace9abfedbd4f31
Carl Parrish <carl.parrish@gmail.com>
2020-03-15 17:46:29 +0000

    loop though object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,26 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 })
 export class AppComponent {
   @ViewChild('display', {static: true}) display: ElementRef;
+  drumKeys: {[key: string]: string} = {
+    'Q': 'RDM_RAW_SY1-Tom01.wav',
+    'W': 'RDM_Raw_MT40-Kick01.wav',
+    'E': 'RDM_Raw_MT40-ClHat.wav'
+  };
+  baseURL = 'https://sourcecodesamurai.s3-us-west-2.amazonaws.com/media/';
   title = 'drum-machine';
 
+  onTap(key: string, fileName:string){
+    this.playMedia(fileName);
+    this.updateDisplay(key);
+  }
+
   playMedia(fileName: string){
-    console.log(fileName);
-    this.display.nativeElement.innerHTML = fileName;
+    let audio = new Audio;
+    audio.src = this.baseURL + 'RDM_Raw_SY1-Tom01.wav';
+    audio.play();
+  }
+
+  updateDisplay(key:string){
+    this.display.nativeElement.innerHTML = key;
   }
 }
